0e4bc89ba0d133a9ccef02c27956e5a9defda9c6
LAPTOP-1M73F46U\JO <joshman411@gmail.com>
2021-11-15 16:30:10 +0200

    today with kris 3

---
+++ cryptolte/cryptolte/Models/Account.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cryptolte.Models
+{
+    public class Account
+    {
+        [Key]
+        public int AccoutId { get; set; }
+        public string Email { get; set; } //a distinct email mapped to each use  account
+        public string accType { get; set; }
+        public string AccoutName { get; set; }
+        public double CurrentAmount { get; set; } //remaining amount after purchases
+        public string wallet { get; set; }
+        public bool confirmed { get; set; }
+        public int clientId { get; set; } //link to the  clients table
+    }
+
+    //enum AccountType
+    //{
+    //    Standard,
+    //    Standrd-Cent
+    //}
+
+    public class AccountType
+    {
+        [Key]
+        public int TypeId { get; set; }
+        public string AccType { get; set; }
+        public double MinDeposit { get; set; }
+    }
+
+    //public enum AccountType
+    //{
+    //    Standard,
+    //    Cent,
+    //    pro
+    //}
+}
---
+++ cryptolte/cryptolte/Models/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cryptolte.Models
+{
+    public class Client
+    {
+        [Key]
+        public int id { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public DateTime dateOfBirth { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public string gender { get; set; }
+        public string maritalStatus { get; set; }
+    }
+}
